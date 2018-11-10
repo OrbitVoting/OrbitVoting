525426a5b3ea1dd38da57f877da7f9b258a87c65
Mark <m.edwardsonx@gmail.com>
2018-11-09 21:29:53 -0800

    Sleep timer

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from main import central
 import time
 
 sleepDuration = 2 #Duration between votecounts, in hours
+y = 15
 
-while(True):
+while(x < y): #Loops over a set number of times, every set number of hours
     print("Process Began!")
 
     central()
     print("Process finished.")
     print("Sleeping " + sleepDuration + " hours.")
     time.sleep(sleepDuration * 3600)
+    y = y+1
