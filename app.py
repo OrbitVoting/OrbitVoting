0bde4309399d56f1c57138aec68bde7ae53d1b7b
Mark <m.edwardsonx@gmail.com>
2018-11-09 20:59:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from main import central
 import time
 
+while(True):
+    print("Process Began!")
 
-print("Process Began!")
-
-central()
-print("Process finished.")
-time.sleep(3600)
-print("Good!")
+    central()
+    print("Process finished.")
+    time.sleep(3600*2)
+    print("Good!")
